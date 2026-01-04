ec798e08486c0860b2efbe21f6a7e9cc118038ad
BE77ION <pawandeepsingh7897@gmail.com>
2026-01-04 23:55:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/navBar"
+import NewsBoard from "./components/NewsBoard"
+
+
+export const App = () => {
+  return (
+    <div>
+      <NavBar/>
+      <NewsBoard/>
+    </div>
+  )
+}
+
+export default App
